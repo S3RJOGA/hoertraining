aa6666277e6b6b3edd28cc5e4ff9cac7b801f0d3
S3RJOGA <sergej89ax@web.de>
2026-06-10 21:10:21 +0200

    Hörtraining PWA hinzufügen

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'hoertraining-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (e.request.method === 'GET' && res.ok) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
